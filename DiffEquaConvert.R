f32a8181d286b7ea45988d31e2d10bde28c30231
Hqin2019 <hqin@sdsu.edu>
2021-12-20 21:48:31 -0800

    Revised.

--- DiffEquaConvert.R
+++ DiffEquaConvert.R
@@ -32,6 +32,26 @@ poslat<- abs(lat-0)<=11
 lon1<- lon[poslon]#length is 400
 lat1<- lat[poslat] #length is 88
 
+#line 18-19 from Matlab code need to be converted
 
+tim1<- as.vector(uv_data$tim)
 
+matlab2POS = function(x, timez = "UTC") {
+  days = x - 719529 	# 719529 = days from 1-1-0000 to 1-1-1970
+  secs = days * 86400 # 86400 seconds in a day
+  # This next string of functions is a complete disaster, but it works.
+  # It tries to outsmart R by converting the secs value to a POSIXct value
+  # in the UTC time zone, then converts that to a time/date string that 
+  # should lose the time zone, and then it performs a second as.POSIXct()
+  # conversion on the time/date string to get a POSIXct value in the user's 
+  # specified timezone. Time zones are a goddamned nightmare.
+  return(as.POSIXct(strftime(as.POSIXct(secs, origin = '1970-1-1', 
+                                        tz = 'UTC'), format = '%Y-%m-%d %H:%M', 
+                             tz = 'UTC', usetz = FALSE), tz = timez))
+}
+date1<- matlab2POS(tim1) #if not specify tz, it will use the current time zone.
+
+ssh_data<- uv_data <- readMat("C:/Users/hniqd/Google Drive/ClimateMath/DifferentialEquaConvert//ssh_14_40day_600_3kkm_fft2_v6.mat")
+str(ssh_data)
+ssh<- ssh_data$ssh
 
